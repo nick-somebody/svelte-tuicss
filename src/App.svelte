67b52e36041826cdf45303ddc70d5042f8d8289c
Nick P <nicholas.pagano1982@gmail.com>
2024-02-04 07:31:21 +1100

    add library and docs builds

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,30 @@
 <script lang="ts">
-  import TuiButton from './lib/components/TuiButton.svelte';
   import TuiBackground from './lib/components/TuiBackground.svelte';
-  import { Background, Color, MildColor } from "./lib/enums"
-  import TuiChart from './lib/components/TuiChart.svelte';
-  import TuiWindow from './lib/components/TuiWindow.svelte';
-  import TuiScreen from './lib/components/TuiScreen.svelte';
-  import TuiTabs from './lib/composites/TuiTabs.svelte';
-  import Tab1 from './docs/tabs/Tab1.svelte';
-  import Tab2 from './docs/tabs/Tab2.svelte';
+  import { Background } from "./lib/enums"
   import TuiGrid from './lib/components/TuiGrid.svelte';
   import Buttons from './docs/Buttons.svelte';
-  import TuiDivider from './lib/components/TuiDivider.svelte';
   import Charts from './docs/Charts.svelte';
   import Inputs from './docs/Inputs.svelte';
   import Layouts from './docs/Layouts.svelte';
   import Modals from './docs/Modals.svelte';
   import Navigation from './docs/Navigation.svelte';
   import Table from './docs/Table.svelte'
-    import Bios from './docs/Bios.svelte';
-    import TuiPanel from './lib/components/TuiPanel.svelte';
+  import Bios from './docs/Bios.svelte';
+  import TuiPanel from './lib/components/TuiPanel.svelte';
 </script>
 
 <main>
 
   <TuiBackground color={Background.BlueWhite}>
-    <TuiPanel fullWidth style="margin:28px 0;">
-      <h1 class="center" aria-label="React TUI">
-        ===========react-tuicss===========
-      </h1>
-    </TuiPanel>
+    <TuiGrid>
+      <TuiPanel fullWidth style="margin:28px 0;">
+        <h1 class="center" aria-label="React TUI">
+          ===========react-tuicss===========
+        </h1>
+      </TuiPanel>
+    </TuiGrid>
     <TuiGrid>
       <div class="examples">
-
         <Buttons />
         <Charts />
         <Inputs />
---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+import "tuicss"
+import "./style.css"
+
+export * from "./components"
+export * from "./composites"
